65ca7bd32b976ab46bef62c1584f3473b266d7fc
nickdos <nickdos@gmail.com>
2014-03-21 05:34:36 +0000

    Working version of generic hub.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/generic-biocache-hub@1277 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

---
+++ grails-app/conf/ApplicationResources.groovy
@@ -0,0 +1,13 @@
+modules = {
+//    application {
+//        resource url:'js/application.js'
+//    }
+
+    // Define your skin module here - it must 'dependsOn' either bootstrap (ALA version) or bootstrap2 (unmodified) and core
+
+    generic {
+        dependsOn 'bootstrap', 'hubCore' //
+        resource url: [dir:'css', file:'generic.css']
+    }
+
+}

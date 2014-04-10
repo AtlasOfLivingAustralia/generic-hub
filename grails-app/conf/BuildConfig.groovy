cd17f5ff6d3dac3d2ac0321f265afe7f6061beff
djtfmartin <djtfmartin@gmail.com>
2014-04-10 02:19:57 +0000

    updatepolicy for maven repo
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/generic-hub@1356 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,7 +45,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
-        mavenRepo "http://maven.ala.org.au/repository/"
+        mavenRepo "http://maven.ala.org.au/repository/" { updatePolicy 'always' }
     }
 
     dependencies {

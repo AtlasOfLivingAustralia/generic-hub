10ad7c85966ede5e2ced98eccf77b3a893eb6d92
nickdos <nickdos@gmail.com>
2014-04-16 06:11:36 +0000

    Updated config var logging in bootstrap.groovy.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/generic-hub@1378 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,8 +7,8 @@ class BootStrap {
         log.info "config.security.cas.bypass = ${grailsApplication.config.security.cas.bypass}"
         log.info "config.skin = ${grailsApplication.config.skin}"
         log.info "config.security.cas.appServerName = ${grailsApplication.config.security.cas.appServerName}"
-        log.info "config.proxy = ${grailsApplication.config.proxy}"
-        log.info "config.test.var = ${grailsApplication.config.test.var}"
+        log.info "config.biocache.baseUrl = ${grailsApplication.config.biocache.baseUrl}"
+        log.info "config.biocache.ajax.useProxy = ${grailsApplication.config.biocache.ajax.useProxy}"
     }
     def destroy = {
     }

a9f0edbd5661469dcdc39718370a3645bd2498ce
Dave Martin <djtfmartin@gmail.com>
2014-09-04 21:10:56 +1000

    bump to version 0.56 of plugin
    bump to version 0.56 of plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,7 +7,7 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.plugins.dir="plugins"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
-//grails.plugin.location.'biocache-hubs' = "../biocache-hubs"
+grails.plugin.location.'biocache-hubs' = "../biocache-hubs"
 
 grails.project.fork = [
     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
@@ -62,6 +62,6 @@ grails.project.dependency.resolution = {
         compile ":cache-headers:1.1.6"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:0.54"
+        runtime ":biocache-hubs:0.56"
     }
 }

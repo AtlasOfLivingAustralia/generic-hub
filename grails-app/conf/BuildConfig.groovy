5be2098bcdcf817af92a61971d65b668075b7866
Dave Martin <djtfmartin@gmail.com>
2015-11-26 15:59:10 +0100

    bumped to new plugin version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -52,6 +52,6 @@ grails.project.dependency.resolution = {
         compile ":cache-headers:1.1.6"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:0.70"
+        runtime ":biocache-hubs:0.73"
     }
 }
